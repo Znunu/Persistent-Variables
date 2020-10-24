5df089f02bace29c5fff3b61bd76d5028ac6f110
Znunu <61394004+Znunu@users.noreply.github.com>
2020-10-24 04:47:08 +0200

    Added make_dict

--- pvars.py
+++ pvars.py
@@ -25,14 +25,14 @@ class Format(enum.Enum):
 class PersistentDict(dict):
 
     def __init__(self, filename, fileformat=Format.PICKLE, dump_args=None):
-        if dump_args is None:
-            self.dump_args = {}
-        else:
-            self.dump_args = dump_args
+        if dump_args is None: self.dump_args = {}
+        else: self.dump_args = dump_args
         self.file_format = fileformat
         self.file_name = filename
         if os.access(filename, os.F_OK):
-            self.load(filename)
+            try: self.load(filename)
+            except ValueError: pass
+        else: raise Exception("File not available")
         dict.__init__(self)
 
     def sync(self):
@@ -50,6 +50,7 @@ class PersistentDict(dict):
 
     def __enter__(self):
         return self
+
 
     def __exit__(self, *exc_info):
         self.sync()
@@ -76,6 +77,17 @@ class PersistentDict(dict):
         raise ValueError("File not in a supported format")
 
 
+class Idict(dict):
+
+    def __init__(self, parent, elems: dict):
+        self._parent = parent
+        super().__init__(elems)
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        self._parent.save()
+
+
 class PVar:
 
     def __init__(self, name, value):
@@ -88,31 +100,33 @@ class PVar:
 
 class ModuleContext:
 
-    def __init__(self, path):
+    def __init__(self, path: str):
         self.auto_save = True
         self.path = path
         self.db = PersistentDict(path, Format.PICKLE)
         self.pvar_index = {}
 
+    def make_dict(self, default: dict, name: str):
+        if name in self.db: i_dict = Idict(self, self.db[name])
+        else: i_dict = Idict(self, default)
+        p_var = PVar(name, lambda: i_dict)
+        self.pvar_index[p_var.name] = p_var
+        return i_dict
+
     def make_var(self, default, callback: typing.Callable):
         code = inspect.getsource(callback)
-        if "def" in code:
-            match = re.search(r"return\s*(\w+)", code)
-        elif "lambda" in code:
-            match = re.search(r":\s*(\w+)", code)
-        else:
-            raise TypeError("Improper function passed")
-        if not match:
-            raise TypeError("Improper function passed")
+        # match = re.search(r"return\s*(\w+)\s*$", code)
+        if "def" in code: raise Exception("Non lambda functions are unsupported for now")
+        elif "lambda" in code: match = re.search(r"([^. ]+)\)", code)
+        else: raise TypeError("Improper function passed")
+        if not match: raise TypeError("Improper function passed")
         var_name = match.group(1)
-        pvar = PVar(var_name, callback)
-        self.pvar_index[pvar.name] = pvar
-        if pvar.name not in self.db:
-            return default
-        else:
-            return self.db[pvar.name]
+        p_var = PVar(var_name, callback)
+        self.pvar_index[p_var.name] = p_var
+        if p_var.name not in self.db: return default
+        else: return self.db[p_var.name]
 
-    def clear(self) -> None:
+    def reset(self) -> None:
         for var in self.db: del self.db[var]
         self.db.sync()
 
@@ -123,31 +137,35 @@ class ModuleContext:
         for name, pvar in self.pvar_index.items(): self.db[name] = pvar.value()
         self.db.sync()
 
-    def configure(self, *, auto_save: bool = None, file_format: Format = None, extra_param: dict = None) -> None:
-        if auto_save:
-            self.auto_save = auto_save
-        if file_format:
-            self.db.file_format = file_format
-        if extra_param:
-            self.db.dump_args = extra_param
+    def configure(self, *, auto_save: bool = False, file_format: Format = False, **dump_args) -> None:
+        self.auto_save = auto_save
+        self.db.file_format = file_format
+        self.db.dump_args = dump_args
 
 
-def get_context(extra_path: typing.Union[str, os.PathLike] = "", **config_params):
+def get_context(*, extra_path: typing.Union[str, os.PathLike] = "", abs_path: typing.Union[str, os.PathLike] = "", **config_params):
+
+    if abs_path:
+        db_path = abs_path
+        db_dir = pathlib.Path(db_path).parent
+    else:
+        frame = inspect.stack()[1]
+        module = inspect.getmodule(frame[0])
+        module_path = pathlib.Path(module.__file__)
+        db_dir = pathlib.Path(module.__file__).parent / os.fspath(extra_path)
+        db_dir.mkdir(parents=True, exist_ok=True)
+        db_path = (db_dir / (module_path.stem + ".pydb")).resolve().__str__()
+
     global _contexts
-    frame = inspect.stack()[1]
-    module = inspect.getmodule(frame[0])
-    module_path = pathlib.Path(module.__file__)
-    db_dir = pathlib.Path(module.__file__).parent / os.fspath(extra_path)
-    db_dir.mkdir(parents=True, exist_ok=True)
-    db_path = (db_dir / (module_path.stem + ".pydb")).resolve().__str__()
-    if not db_dir.parent.exists(): raise Exception(f"{db_path} is not valid")
+    if not db_dir.parent.exists():
+        raise Exception(f"{db_path} is not valid")
     for ctx in _contexts:
-        if db_path == ctx.path:
-            return ctx
-    ctx = ModuleContext(db_path)
-    ctx.configure(**config_params)
-    _contexts.append(ctx)
-    return ctx
+        if db_path == ctx.path: return ctx
+    else:
+        ctx = ModuleContext(db_path)
+        ctx.configure(**config_params)
+        _contexts.append(ctx)
+        return ctx
 
 
 def _clean_up():
